e7ed0a4af601c893a56e13e4b7c7273ed828bc5e
joffrey-bion <joffrey.bion@gmail.com>
2018-09-01 00:33:49 +0200

    Fix default template resource access

--- src/main/kotlin/org/hildan/agenda/generator/Main.kt
+++ src/main/kotlin/org/hildan/agenda/generator/Main.kt
@@ -4,22 +4,33 @@ import java.io.File
 import java.io.InputStream
 
 fun main(args: Array<String>) {
-    val templateFilename = if (args.isEmpty()) "agenda-template.docx" else args[0]
     val agenda = fakeAgenda()
-    val generator = AgendaGenerator { templateInputStream(templateFilename) }
+    val generator = AgendaGenerator(templateProvider(args))
 
     generator.generateAgenda(agenda)
 }
 
-fun templateInputStream(templateFilename: String): InputStream {
+private fun templateProvider(args: Array<String>) = if (args.isEmpty()) {
+    defaultTemplateProvider()
+} else {
+    fileTemplateProvider(args[0])
+}
+
+const val defaultResource = "/agenda-template.docx"
+
+private fun defaultTemplateProvider(): () -> InputStream = {
+    AgendaGenerator::class.java.getResourceAsStream(defaultResource)
+}
+
+private fun fileTemplateProvider(templateFilename: String): () -> InputStream {
     val templateFile = File(templateFilename)
     if (!templateFile.exists()) {
-        System.err.println("The given template file was not found")
+        System.err.println("The given template file $templateFilename was not found")
         System.exit(1)
     } else if (templateFile.isDirectory) {
         System.err.println("The given template file path points to a directory, not a .docx file")
         System.exit(1)
     }
-    return templateFile.inputStream()
+    return { templateFile.inputStream() }
 }
 
