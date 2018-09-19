5528f7b14f5f94e4fde2115703e46dde51ab80e9
Joffrey Bion <joffrey.bion@amadeus.com>
2018-09-19 11:51:32 +0200

    Implement call to server properly

--- hr-buddy-ui/src/app/agenda-generator/agenda-generator.component.ts
+++ hr-buddy-ui/src/app/agenda-generator/agenda-generator.component.ts
@@ -1,5 +1,5 @@
-import {Component, OnInit} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-agenda-generator',
@@ -8,9 +8,11 @@ import {HttpClient} from '@angular/common/http';
 })
 export class AgendaGeneratorComponent implements OnInit {
 
-  planningFile: File | null = null;
+  planningFile?: File = null;
 
-  error: Error | null = null;
+  loading: boolean = false;
+
+  error?: Error = null;
 
   constructor(private http: HttpClient) {
   }
@@ -23,14 +25,19 @@ export class AgendaGeneratorComponent implements OnInit {
   }
 
   onSubmit() {
+    this.loading = true;
     this.error = null;
 
     const endpoint = 'http://localhost:8080/planning';
     const formData: FormData = new FormData();
     formData.append('planningFile', this.planningFile, this.planningFile.name);
-    return this.http.post(endpoint, formData, {}).toPromise()
+
+    return this.http.post(endpoint, formData, {
+      responseType: 'arraybuffer',
+    }).toPromise()
         .then(() => console.log('hey!'))
-        .catch((e) => this.error = e);
+        .catch((e) => this.error = e)
+        .then(() => this.loading = false);
   }
 
 }
