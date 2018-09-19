ceebdd22c037b21b0d4b5d0a4b1ba6a3e89b8b12
jbion <joffrey.bion@amadeus.com>
2018-09-19 09:21:20 +0200

    Remove angular material design

--- hr-buddy-ui/src/app/agenda-generator/agenda-generator.component.ts
+++ hr-buddy-ui/src/app/agenda-generator/agenda-generator.component.ts
@@ -1,17 +1,18 @@
-import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import {Component, OnInit} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
 
 @Component({
   selector: 'app-agenda-generator',
   templateUrl: './agenda-generator.component.html',
-  styleUrls: ['./agenda-generator.component.css']
+  styleUrls: ['./agenda-generator.component.css'],
 })
 export class AgendaGeneratorComponent implements OnInit {
 
   planningFile: File | null = null;
 
-  constructor(private http: HttpClient) {
+  error: Error | null = null;
 
+  constructor(private http: HttpClient) {
   }
 
   ngOnInit() {
@@ -22,15 +23,14 @@ export class AgendaGeneratorComponent implements OnInit {
   }
 
   onSubmit() {
-    this.http.post("http://localhost:8080/planning", {
-      planningFile: this.planningFile
-    });
+    this.error = null;
 
-    // const endpoint = "http://localhost:8080/planning";
-    // const formData: FormData = new FormData();
-    // formData.append('planningFile', this.planningFile, this.planningFile.name);
-    // return this.http
-    //     .post(endpoint, formData, { headers: {} });
+    const endpoint = 'http://localhost:8080/planning';
+    const formData: FormData = new FormData();
+    formData.append('planningFile', this.planningFile, this.planningFile.name);
+    return this.http.post(endpoint, formData, {}).toPromise()
+        .then(() => console.log('hey!'))
+        .catch((e) => this.error = e);
   }
 
 }
--- hr-buddy-ui/src/app/app.module.ts
+++ hr-buddy-ui/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
-import {MatButtonModule, MatCardModule, MatToolbarModule} from '@angular/material';
 import {BrowserModule} from '@angular/platform-browser';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {AgendaGeneratorComponent} from './agenda-generator/agenda-generator.component';
 import {AppComponent} from './app.component';
 
@@ -16,10 +14,6 @@ import {AppComponent} from './app.component';
     BrowserModule,
     FormsModule,
     HttpClientModule,
-    BrowserAnimationsModule,
-    MatButtonModule,
-    MatCardModule,
-    MatToolbarModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
