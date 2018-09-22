cdc90e39e9deb4cb158b4ea4ba6cce5c24a34af7
Joffrey BION <joffrey.bion@gmail.com>
2018-09-22 14:14:42 +0200

    Set different serverUrls for dev and prod

--- hr-buddy-ui/src/app/hr-buddy-client.service.ts
+++ hr-buddy-ui/src/app/hr-buddy-client.service.ts
@@ -1,6 +1,8 @@
 import {HttpClient} from '@angular/common/http';
 import {Injectable} from '@angular/core';
 
+import {environment} from '../environments/environment';
+
 @Injectable({
   providedIn: 'root'
 })
@@ -10,10 +12,11 @@ export class HrBuddyClient {
   }
 
   generateAgendas(planningFile: File): Promise<GenerateAgendasResponse> {
+    const serverUrl = environment.serverUrl;
     const formData: FormData = new FormData();
     formData.append('planningFile', planningFile, planningFile.name);
 
-    return this.http.post<GenerateAgendasResponse>('/planning', formData, {}).toPromise()
+    return this.http.post<GenerateAgendasResponse>(`${serverUrl}/planning`, formData, {}).toPromise()
   }
 }
 
